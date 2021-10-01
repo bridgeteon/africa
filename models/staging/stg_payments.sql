ec573b9d959d3aecb6499a6726b7b679b45e3ddc
bridget@eoncollective.com <bridget@eoncollective.com>
2021-10-01 20:38:19 +0000

    Snowflake walkthrough exercise

--- models/staging/stg_payments.sql
+++
@@ -1,9 +0,0 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    -- amount is stored in cents, convert it to dollars
-    {{ cents_to_dollars('payment_amount', 4) }} as amount,
-    created as created_at
-from {{ source('stripe', 'payment') }}
